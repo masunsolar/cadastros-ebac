eb73b30a7f0f231b945b490f84c5c529df4e0ef8
masun <115043110+masunsolar@users.noreply.github.com>
2023-06-07 17:08:46 -0300

    upload app
    projeto de acordo com o cliente

---
+++ seila.c
@@ -0,0 +1,157 @@
+#include <stdio.h> //Biblioteca de cominicação com o user
+#include <stdlib.h> //Biblioteca de alocação de espaço de memória 
+#include <locale.h> //Alocação de texto por região 
+#include <string.h> //Cuida das string
+
+int cadast(){ //Função cadastro
+	
+	char arquivo[80]; //Variáveis 
+	char cpf[40];
+	char nome[50];
+	char sobrenome[50];
+	char cargo[50];
+	
+	printf ("Digite seu cpf: "); //Infos do cadastro
+	scanf("%s",cpf);
+	
+	strcpy(arquivo,cpf); //copia os valores da string
+	
+	FILE *file; //cria arquivo
+	file = fopen(arquivo,"w"); //pasta
+	fprintf(file,cpf); //salva o valor
+	fclose(file); //fecha arquivo
+	
+	file = fopen(arquivo,"a");
+	fprintf(file,", "); //separar info
+	fclose(file);
+	
+	printf("Digite o nome a ser cadastrado: "); //Infos do cadastro
+	scanf("%s",nome);
+	
+	file = fopen(arquivo,"a");
+	fprintf(file,nome);
+	fclose(file);
+	
+	file = fopen(arquivo,"a");
+	fprintf(file,", "); 
+	fclose(file);
+
+	printf("Digite o sobrenome a ser cadastrado: "); //Infos do cadastro
+	scanf("%s",sobrenome);
+
+	file = fopen(arquivo,"a");
+	fprintf(file,sobrenome);
+	fclose(file);
+	
+	file = fopen(arquivo,"a");
+	fprintf(file,", "); 
+	fclose(file);
+
+	printf("Digite o cargo a ser cadastrado: "); //Infos do cadastro
+	scanf("%s",cargo);
+
+	file = fopen(arquivo,"a");
+	fprintf(file,cargo);
+	fclose(file);
+	
+	file = fopen(arquivo,"a");
+	fprintf(file,", "); 
+	fclose(file);
+	
+	system ("pause");
+}
+
+int consult(){
+	
+		setlocale(LC_ALL, "pt_BR_utf8"); //Definindo linguagem
+			
+	char cpf[40];
+	char conteudo[250];
+	
+	printf("Digite o CPF a ser consultado: ");
+	scanf("%s",cpf);
+	
+	FILE *file;
+	file = fopen(cpf,"r");
+	
+	if(file == NULL){
+		
+		printf("Arquivo não localizado\n");
+		
+	}	
+	
+	while(fgets(conteudo, 250, file) != NULL){
+		
+		printf("\nEssas são as informações do usuário: ");
+		printf("%s", conteudo);
+		printf("\n\n");
+	}
+	
+	system("pause");
+	
+}
+
+int del(){
+	
+	char cpf[40];
+	
+	printf("Qual usuário deseja deletar? Digite o cpf: ");
+	scanf("%s",cpf);
+	
+	remove(cpf);
+	
+	FILE *file;
+	file = fopen(cpf,"r");
+	
+	if(file == NULL){
+		
+	printf("CPF não encontrado...\n");
+	system("pause");
+		
+	}
+}
+
+int main(){
+	
+	int option=0; //Definindo a variavel 
+	int laco=1;
+	
+	for(laco=1;laco=1;){
+		
+		system("cls");
+		
+		setlocale(LC_ALL, "English_united States.1252"); //Definindo linguagem
+		
+		printf (".... Teste de seleção bom ....\n\n"); //Inicio do menu
+		printf ("Escolha a opção do menu:\n\n");
+		printf ("\t1 - Cadastrar um usuário\n");
+		printf ("\t2 - Consultar um usuário\n");
+		printf ("\t3 - Excluir um usuário\n\n"); 
+		printf ("Opção: "); //Fim do menu
+		
+		scanf("%d", &option); //Armazenar escolha
+	
+		system("cls");
+		
+		switch(option){ //Inicio da seleção
+		
+		case 1:
+			cadast();
+			break;
+					
+		case 2:
+			consult();
+			break;
+					
+		case 3:
+			del();
+			break;
+		
+		default:
+			printf("Opção Invalida\n");
+			system("pause");
+			break;
+				
+		} //fim da seleção
+	}
+}
